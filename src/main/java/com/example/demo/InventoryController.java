9b9ed9008b8fff0ba05314471651fd3f68c3edd3
Eleila Bradford <eleila.bradford@gmail.com>
2023-05-03 14:28:41 -0600

    started html and endpoint functionality

--- src/main/java/com/example/demo/InventoryController.java
+++ src/main/java/com/example/demo/InventoryController.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/views")
@@ -14,13 +17,40 @@ public class InventoryController {
 
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @GetMapping("/products")
-    public ModelAndView getUsers() {
-        List<Product> products = productRepository.findAll();
-        ModelAndView modelAndView = new ModelAndView("products");
+    public ModelAndView getProducts(@RequestParam(required = false) Long categoryId) {
+
+        List<Category> categories = categoryRepository.findAll();
+        ModelAndView modelAndView = new ModelAndView("Products");
+        modelAndView.addObject("categories", categories);
+
+        if(categoryId == null){
+            return modelAndView;
+        }
+
+        Optional<Category> category = categoryRepository.findById(categoryId);
+        if(!category.isPresent()){
+            return modelAndView;
+        }
+
+        List<Product> products = productRepository.findByCategory(category.get());
         modelAndView.addObject("products", products);
         return modelAndView;
+    }
+
+    @GetMapping("/create-category")
+    public ModelAndView getCreateCategory(){
+        ModelAndView createCategory = new ModelAndView("CreateCategory");
+        return createCategory;
+    }
+
+    @PostMapping("/create-category")
+    public String createCategory(Category category){
+        Category newCategory = categoryRepository.save(category);
+        return "redirect:/views/products?categoryId=" + newCategory.getCategory_id();
     }
 
 }
--- src/main/java/com/example/demo/ProductRepository.java
+++ src/main/java/com/example/demo/ProductRepository.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategory(Category category);
+
 }
